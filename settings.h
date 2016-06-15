8fd3e6f69ec2ee1a3575d4704b998f9c9f5eae1b
Bruno Luiz da Silva <contato@brunoluiz.net>
2016-06-15 20:53:11 +0100

    Using LBP Cascades (theorically better than Haar, but still needs testing)

--- settings.h
+++ settings.h
@@ -3,8 +3,8 @@
 
 #define WINDOW_NAME "Face Detection Sandbox"
 
-#define MODEL_CASCADE "/usr/local/share/OpenCV/haarcascades/haarcascade_frontalface_default.xml"
-//#define MODEL_CASCADE "/usr/local/share/OpenCV/lbpcascades/lbpcascade_frontalface.xml"
+#define MODEL_CASCADE "/usr/local/share/OpenCV/lbpcascades/lbpcascade_frontalface.xml"
+//#define MODEL_CASCADE "/usr/local/share/OpenCV/haarcascades/haarcascade_frontalface_default.xml"
 //#define MODEL_CASCADE "/usr/local/share/OpenCV/lbpcascades/lbpcascade_profileface.xml"
 
 #endif // SETTINGS_H
